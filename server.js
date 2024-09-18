a0627955f0c936922e5e2ff09872cf85fc503225
Thomas PINEAU <thomas.pineau93@proton.me>
2024-09-18 02:14:28 +0200

    Performance is good now thanks to preload of the first image and lazy load of the rest. Could be minifying other stuff

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const compression = require('compression');
+const shrinkRay = require('shrink-ray-current');
 const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(shrinkRay());
+app.use(compression());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
