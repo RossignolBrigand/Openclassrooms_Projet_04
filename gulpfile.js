d98e961e3cc0a347fd68e7d17978090d21930d2d
Thomas PINEAU <thomas.pineau93@proton.me>
2024-09-20 16:32:35 +0200

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ gulp.task("styles", function() {
     return gulp.src('./assets/css/*.css')
     .pipe(concat('combined.min.css'))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/dist'));
 });
 
 gulp.task('scripts', function() {
     return gulp.src('./assets/js/*.js')
     .pipe(concat('combined.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./assets/js'));
+    .pipe(gulp.dest('./assets/dist'));
 });
 
 gulp.task("default", gulp.parallel("styles", "scripts"));
