8857b7e7abacc022530757e8be0676d2030b9891
Thomas PINEAU <thomas.pineau93@proton.me>
2024-09-20 15:01:01 +0200

    Cleanup gulp and fonts issues

--- assets/js/schema.js
+++
@@ -1,9 +0,0 @@
-fetch("./assets/schemas/schema.json")
-.then(response => response.json())
-.then(data => {
-  let script = document.createElement('script');
-  script.type = 'application/ld+json';
-  script.text = JSON.stringify(data);
-  document.head.appendChild(script);
-})
-.catch(error => console.error('Error loading structured data:', error));
--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ gulp.task("styles", function() {
     return gulp.src('./assets/css/*.css')
     .pipe(concat('combined.min.css'))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./assets/dist'));
+    .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('./assets/js/defer/*.js')
+    return gulp.src('./assets/js/*.js')
     .pipe(concat('combined.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./assets/dist'));
+    .pipe(gulp.dest('./assets/js'));
 });
 
 gulp.task("default", gulp.parallel("styles", "scripts"));
